3794bf610375dc678253264a8b909035d4f76a99
Jordan Winslow <admin@jordanwinslow.me>
2019-07-03 20:48:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -29,5 +29,5 @@ function incrementVariable() {
 function doWhileLoop(integer){
   do {
     console.log("I run once regardless")
-  } while (incrementVariable() < integer)
+  } while (incrementVariable() <= integer)
   }
